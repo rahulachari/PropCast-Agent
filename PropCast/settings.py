0a53f5b858569919edb32e74199bdab1f929bf63
rahulachari <rahulachariyc@gmail.com>
2026-05-28 18:32:46 +0530

    Fix CSRF and production settings

--- PropCast/settings.py
+++ PropCast/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qwh=cnahgw@+*hxh0b7w%jv+4q#lylp2e1ve&gipiyk&^0orz6'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-qwh=cnahgw@+*hxh0b7w%jv+4q#lylp2e1ve&gipiyk&^0orz6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['https://propcast-oyan.onrender.com']
 
 # Application definition
 
@@ -116,13 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
